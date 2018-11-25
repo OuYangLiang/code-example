6998aa592b553980f5ac147ef52faaff6c298d3d
OuYang Liang <ouyanggod@gmail.com>
2018-11-26 00:16:33 +0800

    springboot: AOP support.

---
+++ springboot-demo/src/main/java/com/personal/test/demo/aop/DemoAop.java
@@ -0,0 +1,50 @@
+package com.personal.test.demo.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class DemoAop {
+    
+    private static final Logger log = LoggerFactory.getLogger(DemoAop.class);
+    
+    @Pointcut("execution(* com.personal.test.demo.*.dao.*.*(..))")
+    public void dao() {
+    }
+    
+    @Pointcut("execution(* com.personal.test.demo.web.*.*(..))")
+    public void controller() {
+    }
+
+    @Before("controller()")
+    public void beforeController(JoinPoint joinPoint) {
+        log.info("Before: " + joinPoint.getSignature().getName());
+    }
+    
+    @Before("dao()")
+    public void before(JoinPoint joinPoint) {
+        log.info("Before: " + joinPoint.getSignature().getName());
+    }
+    
+    @After("dao()")  
+    public void after(JoinPoint joinPoint){  
+        log.info("After: " + joinPoint.getSignature().getName());  
+    }
+    
+    @Around("dao()")  
+    public Object around(ProceedingJoinPoint pjp) throws Throwable{  
+        log.info("Around start: " + pjp.getSignature().getName());
+        Object rlt = pjp.proceed();
+        log.info("Around end: " + pjp.getSignature().getName());
+        return rlt;
+    }
+}
