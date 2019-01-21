b9b7b07a97920ebf6f70dea72d3b903a1541acb4
OuYang Liang <ouyanggod@gmail.com>
2019-01-22 00:46:58 +0800

    refresh with spring cloud config

--- spring-cloud/eureka-client/src/main/java/com/personal/test/demo/eurekaclient/web/DcController.java
+++ spring-cloud/eureka-client/src/main/java/com/personal/test/demo/eurekaclient/web/DcController.java
@@ -3,10 +3,12 @@ package com.personal.test.demo.eurekaclient.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DcController {
 
     @Autowired
